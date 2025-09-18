f5838eeb01be4b9c53e5d0169c3181fee606c460
chrish <chris.d.howells@gmail.com>
2025-09-18 15:52:13 +0100

    Initial commit

--- resources/modules/vpc/firewall_rules.tf
+++ resources/modules/vpc/firewall_rules.tf
@@ -128,7 +128,7 @@ resource "google_compute_firewall" "allow_iap" {
   direction = "INGRESS"
   name      = "${var.name}-network-iap-ingress"
   network   = google_compute_network.vpc.id
-  priority  = 1001
+  priority  = 999
   source_ranges = [
     "35.235.240.0/20"
   ]
